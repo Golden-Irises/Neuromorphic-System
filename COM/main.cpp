7fe81fcb3db82bff66fb3348a6d5f91558e630a0
AuroraKarow <karowmisaki@outlook.com>
2023-05-23 17:56:23 +0800

    Update demo for COM

--- COM/main.cpp
+++ COM/main.cpp
@@ -5,6 +5,8 @@
 #define NS_BEGIN    namespace NS_NAME {
 #define NS_END      }
 #define NS_MSG      false
+#define DCB_FLAG    true
+#define DCB_BUF_LEN 0x0010
 
 NS_BEGIN
 
@@ -23,7 +25,7 @@ bool dcb_open_port(HANDLE h_port, int idx = NULL) {
                         NULL);
     if (h_port == INVALID_HANDLE_VALUE) return false;
 
-    // timeout interval for IO
+    // timeout interval for IO device (ms)
     COMMTIMEOUTS timeouts {0};
     timeouts.ReadIntervalTimeout         = 50;
     timeouts.ReadTotalTimeoutConstant    = 50;
@@ -111,8 +113,34 @@ bool dcb_read(HANDLE h_port, char *buf_dest, int buf_len) {
 NS_END
 
 using namespace std;
+using namespace NS_NAME;
 
 int main(int argc, char *argv[], char *envp[]) {
     cout << "hello, world." << endl;
-    return EXIT_SUCCESS;
+
+    // 端口句柄
+    HANDLE h_port;
+    // 打开端口
+    if (!dcb_open_port(h_port)) return EXIT_FAILURE;
+
+    // 端口参数
+    if (!dcb_port_params(h_port,    // 端口句柄
+                         19200,     // 波特率 19200
+                         20,        // 比特数 20
+                         1,         // 停止位 1
+                         NOPARITY)) // 无奇偶校验
+                         return EXIT_FAILURE;
+
+    // Windows 串口消息
+    while (DCB_FLAG) {
+        // 端口数据
+        char buffer [DCB_BUF_LEN];
+        dcb_read(h_port, buffer, DCB_BUF_LEN);
+        // 休眠 1s
+        _sleep(1000);
+    }
+
+    // 销毁句柄
+    if (dcb_close_port(h_port)) return EXIT_SUCCESS;
+    return EXIT_FAILURE;
 }

61744b143d921b2187d10e756b4f146b0bc90746
AuroraKarow <karowmisaki@outlook.com>
2023-10-08 22:23:02 +0800

    `kokkoro_dataset` data show testing

--- SRC/ARCHIVE/kokkoro_data.hpp
+++ SRC/ARCHIVE/kokkoro_data.hpp
@@ -1,54 +1,30 @@
 KOKKORO_BEGIN
 
-uint64_t kokkoro_lbl_mask(char syb) { switch (syb) {
-    case '+': return 0x0000;
-    case '-': return 0x0001;
-    case 'x': return 0x0002;
-    case '/': return 0x0003;
-    default: return 9;
+// unsafe
+void kokkoro_dcb_vect_mask(int *ans, char *src) { for (auto i = 0; i < kokkoro_dcb_mask; ++i) for (auto j = 0; j < kokkoro_dcb_mask; ++j) {
+    ans[i + j * kokkoro_dcb_mask] = src[j] & kokkoro_dcb_mask;
+    src[j]                      >>= kokkoro_dcb_bitsz;
 } }
 
-struct kokkoro_data {
-    kokkoro_set<kokkoro_matrix> elem;
-    kokkoro_set<uint64_t> lbl;
-};
-
-kokkoro_data kokkoro_train_data(const std::string &csv_load_path) {
-    auto data_raw = csv_in(csv_load_path);
-    kokkoro_data ans;
-    if (!data_raw.length < 1) return ans;
-    ans.elem.init(data_raw.length - 1);
-    ans.lbl.init(data_raw.length - 1);
-    for (auto i = 0ull; i < ans.lbl.length; ++i) {
-        ans.lbl[i]  = std::atof(data_raw[i + 1][9].c_str());
-        ans.elem[i] = {9, 1};
-        for (auto j = 0; j < 9; ++j) ans.elem[i].index(j) = kokkoro_lbl_mask(data_raw[i + 1][j][0]);
-    }
-    return ans;
-}
-
-void kokkoro_dcb_vect_mask(int ans[9], char src[3]) { for (auto i = 0; i < 3; ++i) {
-    ans[i]     = (src[i] & kokkoro_dcb_mask);
-    ans[i + 3] = (src[i] & kokkoro_dcb_mask);
-    ans[i + 6] = (src[i] & kokkoro_dcb_mask);
-    src[i]   >>= kokkoro_dcb_bitsz;
-    src[i]   >>= kokkoro_dcb_bitsz;
-    src[i]   >>= kokkoro_dcb_bitsz;
-} }
-
-template <typename = std::enable_if_t<kokkoro_dcb_bufsz % kokkoro_dcb_thdsz == 0>>
 bool kokkoro_dcb_read_thread(dcb_hdl dcb_port, int baudrate = CBR_9600, int buf_sz = 1024) {
     if (!dcb_startup(dcb_port, 3, baudrate, 6, ONESTOPBIT, NOPARITY, false, buf_sz, buf_sz)) return false;
-    constexpr auto proc_sz = kokkoro_dcb_bufsz / kokkoro_dcb_thdsz;
     #if kokkoro_dcb_msg
     std::printf("[Startup DCB port.]");
     #endif
-    char dcb_buffer[kokkoro_dcb_bufsz] = {0};
-    auto buffer_len = 0;
+    char dcb_buf[4]  = {0};
+    int  buffer_len  = 0,
+         data_tmp[9] = {0};
     do {
-        buffer_len = dcb_read(dcb_port, dcb_buffer, kokkoro_dcb_bufsz);
-        if (!buffer_len) return false;
-        
+        buffer_len = dcb_read(dcb_port, dcb_buf, 4);
+        if (!buffer_len || (*dcb_buf) == kokkoro_dcb_idle) return false;
+        // #if kokkoro_dcb_msg
+        char bi_str[10] = {0};
+        for (auto i = 0; i < 4; ++i) {
+            itoa(dcb_buf[i], bi_str, 2);
+            std::printf("[00%s]", bi_str);
+        }
+        // #endif
+        kokkoro_dcb_vect_mask(data_tmp, dcb_buf);
     } while (buffer_len);
     return true;
 }
--- SRC/ARCHIVE/kokkoro_data_main.cpp
+++ SRC/ARCHIVE/kokkoro_data_main.cpp
@@ -3,9 +3,7 @@
  */
 #pragma once
 
-#define kokkoro_dcb_msg     false
-#define kokkoro_dcb_bufsz   300
-#define kokkoro_dcb_thdsz   4
+#define kokkoro_dcb_msg true
 
 #include <iostream>
 #include "../../ANN/kokkoro"
@@ -15,6 +13,15 @@ using namespace kokkoro;
 using namespace std;
 
 int main(int argc, char *argv[], char *envp[]) {
-    
+    char test[] = {0x1d, 0x3a, 0x14, 0x3f};
+    for (auto i = 0; i < 4; ++i) cout << '[' << bitset<8>(test[i]) << ']'; cout << endl;
+    int deci[9] = {0};
+    kokkoro_dcb_vect_mask(deci, test);
+    for (auto i = 0; i < 9; i += 3) {
+        cout << "[  ";
+        for (auto j = 3; j; --j) cout << ' ' << deci[i + j - 1];
+        cout << ']';
+    }
+    cout << "[idle]" << endl;
     return EXIT_SUCCESS;
 }

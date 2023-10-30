aa993356ef04361d7821d6c9eda4d77c4f2d698a
AuroraKarow <karowmisaki@outlook.com>
2023-10-30 22:50:59 +0800

    Create master machine necessary head file

--- main.cpp
+++ main.cpp
@@ -4,11 +4,8 @@
 
 #pragma once
 
-#pragma comment(lib, "Kernel32.lib")
-#include <windows.h>
-
 #include <iostream>
-#include "ANN/kokkoro"
+#include "kokkoro_master"
 
 using namespace std;
 
@@ -16,10 +13,7 @@ int main(int argc, char *argv[], char *envp[]) {
     cout << "Welcome back home, my master." << endl;
     auto tm_start = kokkoro_chrono_time_point;
 
-    // powercfg /batteryreport /output "..\html_file_path.html"
-    SYSTEM_POWER_STATUS power_stat;
-    GetSystemPowerStatus(&power_stat);
-    cout << int(power_stat.BatteryLifePercent) << '%' << endl;
+    cout << "hello, world." << endl;
 
     cout << kokkoro_chrono_time_point - tm_start << "ms" << endl;
     return EXIT_SUCCESS;

88a6b30044850c10517a2c0114df9fc12bfa4121
AuroraKarow <karowmisaki@outlook.com>
2024-07-12 00:02:14 +0800

    Correct bugs

--- SRC/kokkoro_data.hpp
+++ SRC/kokkoro_data.hpp
@@ -165,7 +165,7 @@ void kokkoro_array_save_thread(kokkoro_array_handle &kokkoro_handle, bool zero_a
             #if kokkoro_data_save
             std::cout << "[Symbol][+(1) -(2) x(3) /(4)]: ";
             std::cin.clear();
-            std::cin.ignore((numeric_limits<streamsize>::max)(), '\n');
+            std::cin.ignore((std::numeric_limits<streamsize>::max)(), '\n');
             ctrl_key = std::cin.get();
             #endif
 
